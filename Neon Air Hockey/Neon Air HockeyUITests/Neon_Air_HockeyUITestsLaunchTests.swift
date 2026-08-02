081ed21b32e386082c4cb310520319c208e4fae9
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:07:56 -0400

    Add Neon Air Hockey/Neon Air HockeyUITests/Neon_Air_HockeyUITestsLaunchTests.swift

---
+++ Neon Air Hockey/Neon Air HockeyUITests/Neon_Air_HockeyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Neon_Air_HockeyUITestsLaunchTests.swift
+//  Neon Air HockeyUITests
+//
+//  Created by Elliot Williams on 2025-07-03.
+//
+
+import XCTest
+
+final class Neon_Air_HockeyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
