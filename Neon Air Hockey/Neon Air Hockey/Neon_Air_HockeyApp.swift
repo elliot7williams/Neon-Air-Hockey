829cfbab1b8aee12bc204853bbeca56b6ffee10d
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:07:46 -0400

    Add Neon Air Hockey/Neon Air Hockey/Neon_Air_HockeyApp.swift

---
+++ Neon Air Hockey/Neon Air Hockey/Neon_Air_HockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neon_Air_HockeyApp.swift
+//  Neon Air Hockey
+//
+//  Created by Elliot Williams on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct Neon_Air_HockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NetworkedNeonAirHockey()
+        }
+    }
+}
