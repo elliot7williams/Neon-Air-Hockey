218e940e8aa044282454d5deabbf6775a1f91b0c
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:07:43 -0400

    Add Neon Air Hockey/Neon Air HockeyTests/Neon_Air_HockeyTests.swift

---
+++ Neon Air Hockey/Neon Air HockeyTests/Neon_Air_HockeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Neon_Air_HockeyTests.swift
+//  Neon Air HockeyTests
+//
+//  Created by Elliot Williams on 2025-07-03.
+//
+
+import Testing
+@testable import Neon_Air_Hockey
+
+struct Neon_Air_HockeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
